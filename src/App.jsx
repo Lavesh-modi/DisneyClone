33b79fdce8e2394621ca042d9f1666c1dfbb6c7e
lavesh <laveshmodiag@gmail.com>
2024-11-14 13:28:55 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-gray-500 h-[100vh] w-[100vw]'>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
